f560823db03f1fab47cb7b855ff01df477833dd9
dani.camp <bboydanee@gmail.com>
2020-02-02 07:40:35 +0000

    userlogin

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only:[:tasks]
+  before_action :correct_user, only:[:show]
   
   def index
     if logged_in?
-      @task = current_user.tasks.build
+      #@task = current_user.tasks.build
       @tasks = current_user.tasks.order(:id)
       
     end
